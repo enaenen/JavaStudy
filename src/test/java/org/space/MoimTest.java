e76f917bb517191d956be57754c040309ba74373
Space <enaenen@naver.com>
2022-04-02 00:37:53 +0900

    codecoverage

---
+++ src/test/java/org/space/MoimTest.java
@@ -0,0 +1,18 @@
+package org.space;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoimTest{
+
+    @Test
+    public void isFull(){
+        Moim moim = new Moim();
+        moim.maxNumberOfAttendees = 100;
+        moim.numberOfEnrollment = 10;
+
+        Assert.assertFalse(moim.isEnrollmentFull());
+    }
+
+}
